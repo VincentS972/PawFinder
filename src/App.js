bb1234a46476363314f00481bd2098f3ee693eb0
cpeppsi <caepps28@gmail.com>
2023-09-17 12:36:48 -0400

    New Home component with NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import React from "react";
-import Navbar from './Components/NavBar';
-// import Home from "./Components/Home";
+import Home from "./Components/Home";
 // import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Home />
     </div>
   );
 }
