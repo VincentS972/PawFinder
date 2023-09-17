0ae7e2029e2009fbbb423c1fa0be061ce7d4c768
cpeppsi <caepps28@gmail.com>
2023-09-17 12:03:47 -0400

    Installing packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Navbar from './Components/NavBar';
-import Home from "./Components/Home";
-import Footer from "./Components/Footer";
+// import Navbar from './Components/NavBar';
+// import Home from "./Components/Home";
+// import Footer from "./Components/Footer";
 
 function App() {
   return (
