a4fa7a63eb2ef81f78e7c0c930a15d3b227fc877
cpeppsi <caepps28@gmail.com>
2023-09-17 12:33:38 -0400

    Installed react-bootstrap-validation and added NavBar to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-// import Navbar from './Components/NavBar';
+import React from "react";
+import Navbar from './Components/NavBar';
 // import Home from "./Components/Home";
 // import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-     
+      <Navbar />
     </div>
   );
 }
