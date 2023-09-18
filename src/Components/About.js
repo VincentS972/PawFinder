0b4cebd12d38918ef7d0ac95b8a98f59f662d1e6
VincentS972 <vincentsechi@gmail.com>
2023-09-17 21:43:32 -0400

    Added NavBar, footer. Created pages

---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+ 
+
+const About = () => {
+    return (
+      <div className="bg" style={{ textAlign:"center" }} >
+        <h1>This is the About page</h1>
+      </div>
+    );
+  };
+  export default About;
+  
---
+++ src/Components/Fosters.js
@@ -0,0 +1,11 @@
+
+
+const Fosters = () => {
+    return (
+      <div className="bg" style={{ textAlign:"center" }}>
+        <h1>This is the fosters page!</h1>
+      </div>
+    );
+  };
+  export default Fosters;
+  
