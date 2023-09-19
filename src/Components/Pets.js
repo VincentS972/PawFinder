c7fc19a736bc71531cd3bbe5581ed6e23dfbd85c
Finngodumb <finny12349@gmail.com>
2023-09-19 19:50:09 -0400

    adding placeholder card

--- src/Components/Pets.js
+++ src/Components/Pets.js
@@ -1,10 +1,32 @@
 
-const Pets = () => {
-    return (
-      <div className="bg" style={{textAlign:"center"}} >
-        <h1>This is the pet page</h1>
-      </div>
-    );
-  };
-  export default Pets;
+//const Pets = () => {
+   // return (
+     // <div className="bg" style={{textAlign:"center"}} >
+     //   <h1>This is the pet page</h1>
+     // </div>
+   // );
+ // };
+  import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function card1() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://images.pexels.com/photos/4056462/pexels-photo-4056462.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+      <Card.Body>
+        <Card.Title>random placeholder dog</Card.Title>
+        <Card.Text>
+          Come adopt this dog because hes super cool!!!
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    
+  );
+}
+
+
+
+  export default card1;
+ 
   
