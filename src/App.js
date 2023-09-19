1ddb5416e6fdcccdb047eeb78c7b21a7de742641
VincentS972 <vincentsechi@gmail.com>
2023-09-19 18:38:27 -0400

    Added NavBar route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from './Components/NavBar';
-import Home from "./Components/Home";
-import Footer from "./Components/Footer";
+// import Home from "./Components/Home";
+// import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-     
+     <Router>
+        <Navbar />
+    
+        <Routes>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/Pets" element={<Pets />} />
+          <Route path="/Fosters" element={<Fosters />} />
+          <Route path="/PetCare" element={<PetCare />} />
+          <Route path="/About" element={<About />} /> */}
+        </Routes>
+        {/* <Footer /> */}
+      </Router>
     </div>
   );
 }
