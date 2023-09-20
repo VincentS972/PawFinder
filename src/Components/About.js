0ed1d54515ed9f072196d92cefd7027ab5d099db
VincentS972 <vincentsechi@gmail.com>
2023-09-19 23:30:46 -0400

    Added logo to the navbar and the tab

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,44 @@
+import Card from 'react-bootstrap/Card';
+import MissionStatement from './MissionStatement';
 
-const About = () => {
+function About() {
     return (
-      <div className="bg"style={{ textAlign:"center" }} >
-        <h1>This is the About page</h1>
-      </div>
-    );
-  };
-  export default About;
-  
+      <div className='bg'>
+        <div>
+            <div style={{ textAlign:"center"}}>
+                <img
+                src="https://unsplash.com/photos/N_G2Sqdy9QY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                alt="Woman with cat"
+                className="object-fit-cover img-fluid m0"
+                />
+                <div className='p-2' style={{
+                position:'absolute', 
+                bottom:'0px', 
+                zIndex:'1', 
+                marginLeft: 'auto', 
+                marginRight:'auto', 
+                width:'100%', 
+                textAlign: 'center', 
+                color:'black', 
+                }}>
+                Photo by <a href="https://unsplash.com/@bto16180?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Humberto Arellano</a> on <a href="https://unsplash.com/photos/N_G2Sqdy9QY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+            </div>
+            <div style={{ textAlign:"center" }}>
+                <Card style={{ 
+                    display:'inline-block',
+                    border:'2px solid black', 
+                    margin: '15px', 
+                    backgroundColor:"transparent",
+                    width: '70%',
+                    padding: '15px'
+                  }}>
+                    {<MissionStatement/>}
+                </Card>
+            </div>
+        </div>
+       </div>
+    )
+}
+
+export default About;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,24 +1,86 @@
-import Nav from "react-bootstrap/Nav";
+import { NavbarBrand } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import Container from 'react-bootstrap/Container';
+import { Nav } from "react-bootstrap";
 
 function NavBar() {
     return (
         <div style={{ top:"0", position:"sticky", zIndex:"1" }}>
-            <Nav style={{ backgroundColor:"lightgrey", fontSize:"40px" }} fill variant="tabs" defaultActiveKey="/home">
-                <Nav.Item>
-                <Nav.Link style={{ color: 'inherit', textDecoration: 'inherit' }} href="/">Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                <Nav.Link style={{ color: 'inherit', textDecoration: 'inherit'}} href="/Pets">Our Pets</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                <Nav.Link style={{ color: 'inherit', textDecoration: 'inherit'}} href="/Fosters">Our Fosters</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                <Nav.Link style={{ color: 'inherit', textDecoration: 'inherit'}} href="/PetCare">Pet Care</Nav.Link>
-                </Nav.Item>
-            </Nav>
-      </div>
+            <Nav className="justify-content-center" style={{ backgroundColor:"lightgrey", fontSize:"40px" }}  defaultActiveKey="/home">
+                    <Navbar className="bg-body-tertiary">
+                    <Container>
+                      <Navbar.Brand href="/">
+                        <img
+                          alt=""
+                          src="./images/PawFinderLogo.png"
+                          width="50"
+                          height="50"
+                          className="d-inline-block align-top"
+                        />
+                      </Navbar.Brand>
+                    </Container>
+                  </Navbar>
+                <Nav.Item >
+                 <Nav.Link style={{ color: 'inherit', textDecoration: 'inherit' }} href="/">Home</Nav.Link>
+                 </Nav.Item>
+                 <Nav.Item>
+                 <Nav.Link style={{ color: 'inherit', textDecoration: 'inherit'}} href="/Pets">Our Pets</Nav.Link>
+                 </Nav.Item>
+                 <Nav.Item>
+                 <Nav.Link style={{ color: 'inherit', textDecoration: 'inherit'}} href="/Fosters">Our Fosters</Nav.Link>
+                 </Nav.Item>
+                 <Nav.Item>
+                 <Nav.Link style={{ color: 'inherit', textDecoration: 'inherit'}} href="/PetCare">Pet Care</Nav.Link>
+                 </Nav.Item>
+           </Nav>
+       </div>
     );
   }
+
+//   return (
+//     <>
+//       <Navbar className="bg-body-tertiary">
+//         <Container>
+//           <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+//         </Container>
+//       </Navbar>
+//       <br />
+//       <Navbar className="bg-body-tertiary">
+//         <Container>
+//           <Navbar.Brand>Brand text</Navbar.Brand>
+//         </Container>
+//       </Navbar>
+//       <br />
+//       <Navbar className="bg-body-tertiary">
+//         <Container>
+//           <Navbar.Brand href="#home">
+//             <img
+//               src="/img/logo.svg"
+//               width="30"
+//               height="30"
+//               className="d-inline-block align-top"
+//               alt="React Bootstrap logo"
+//             />
+//           </Navbar.Brand>
+//         </Container>
+//       </Navbar>
+//       <br />
+//       <Navbar className="bg-body-tertiary">
+//         <Container>
+//           <Navbar.Brand href="#home">
+//             <img
+//               alt=""
+//               src="./images/PawFinderLogo.png"
+//               width="30"
+//               height="30"
+//               className="d-inline-block align-top"
+//             />{' '}
+//             React Bootstrap
+//           </Navbar.Brand>
+//         </Container>
+//       </Navbar>
+//     </>
+//   );
+// }
 
 export default NavBar;
