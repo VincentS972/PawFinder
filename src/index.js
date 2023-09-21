0af9f3672b16f9e37b9866573a4546be6fabdb5b
cpeppsi <caepps28@gmail.com>
2023-09-21 15:54:02 -0400

    Updated seed data, added webpack config files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const petRoutes = require('./controllers/pet');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use('/pets', petRoutes);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
 
+// DB Connection
+mongoose.connect(process.env.MONGO_URI, { UseNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('DB connected'))
+  .catch(err => console.error(err));
+
+const PORT = process.env.PORT 
+
+app.listen(PORT, console.log(`listening on port ${PORT}`))
