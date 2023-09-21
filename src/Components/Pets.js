91811812b7e5c9314d9814c03414a6c15f05f7aa
Finngodumb <finny12349@gmail.com>
2023-09-21 19:58:02 -0400

    different cards

--- src/Components/Pets.js
+++ src/Components/Pets.js
@@ -6,27 +6,37 @@
      // </div>
    // );
  // };
-  import Button from 'react-bootstrap/Button';
+  //import Button from 'react-bootstrap/Button';
+//import Card from 'react-bootstrap/Card';
+
+
+
 import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function card1() {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://images.pexels.com/photos/4056462/pexels-photo-4056462.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+      <Card.Img variant="top" src="https://images.pexels.com/photos/160846/french-bulldog-summer-smile-joy-160846.jpeg?auto=compress&cs=tinysrgb&w=1600" />
       <Card.Body>
-        <Card.Title>random placeholder dog</Card.Title>
+        <Card.Title>Juno</Card.Title>
         <Card.Text>
-          Come adopt this dog because hes super cool!!!
+          Juno is a fun and playful dog who likes belly rubs!
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>French Bulldog</ListGroup.Item>
+        <ListGroup.Item>Foster Parent: Alex</ListGroup.Item>
+        <ListGroup.Item>Age: 3</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Adopt me!</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
       </Card.Body>
     </Card>
-    
   );
 }
 
-
-
-  export default card1;
+export default card1;
  
   
