e5450aebfa71d63162a5bab6262441cc5772bfd9
cpeppsi <caepps28@gmail.com>
2023-09-24 09:38:49 -0400

    update webpack

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import React from "react";
 import Navbar from './Components/NavBar';
 import Home from "./Components/Home";
