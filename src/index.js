3733cb79541a3d14236a626f0b779b2ea52f4c86
cpeppsi <caepps28@gmail.com>
2023-09-24 00:29:52 -0400

    debugging

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-require('dotenv').config()
 const express = require('express')
-// const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+// const bodyParser = require('body-parser')******CAUSING 2 ERRORS********
 const cors = require('cors')
+// require('dotenv').config() *****CAUSING 3 ERRORS*******
+
 const app = express();
 
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
 
 // Middleware
 app.use(cors())
@@ -27,9 +28,9 @@ root.render(
 );
 
 // DB Connection
-// mongoose.connect(process.env.MONGO_URI, { UseNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('DB connected'))
-//   .catch(err => console.error(err))
+mongoose.connect(process.env.MONGO_URI, { UseNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('DB connected'))
+  .catch(err => console.error(err))
 
 const PORT = process.env.PORT 
 
