ef2753473c26b43aa16dede55e38d2f3216d0559
Mayna-B <me2be2014@gmail.com>
2023-09-26 19:19:06 -0400

    fixing typos in image paths

--- src/Components/PetCare.js
+++ src/Components/PetCare.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DogCare from "./DogCare";
+import CatCare from "./CatCare";
 
 function PetCare() {
   return (
@@ -8,7 +9,7 @@ function PetCare() {
         <h1>Pet Care Tips for New Pet Owners</h1>
       </div>  
       <div style={{ marginTop:"5px", textAlign:"center" }}>
-        <Image style={{ height:"30%", width:"35%" }} src={require('./public.images/Dog1')} thumbnail />
+        <Image style={{ height:"30%", width:"35%" }} src={require('./public/images/Dog1')} thumbnail />
       </div>
       <div style={{ textAlign:"center" }}>
                 <Card style={{ 
@@ -23,7 +24,7 @@ function PetCare() {
                 </Card>
       </div>
       <div style={{ marginTop:"5px", textAlign:"center" }}>
-        <Image style={{ height:"30%", width:"35%" }} src={require('./public.images/Cat1')} thumbnail />
+        <Image style={{ height:"30%", width:"35%" }} src={require('./public/images/Cat1')} thumbnail />
       </div>
       <div style={{ textAlign:"center" }}>
                 <Card style={{ 
