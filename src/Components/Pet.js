dd540de448225c502e0836d47e8562490f22c4a9
VincentS972 <vincentsechi@gmail.com>
2023-09-26 20:45:03 -0400

    Changed bg style

--- src/Components/Pet.js
+++ src/Components/Pet.js
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
 import React from "react";
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 function Pet() {
+  
   const [pets, setPets] = useState([])
   
 
@@ -72,51 +72,6 @@ function Pet() {
     </div>
   );
 }
-
-// const Pet = () => {
-//   const navigate = useNavigate();
-//   const [pet, setPet] = useState(undefined);
-
-//   const { id } = useParams();
-//   const URL = `${process.env.REACT_APP_BACKEND_URI}/pet/${id}`;
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const response = await fetch(URL);
-//       const data = await response.json();
-//       setPet(data);
-//     };
-//     fetchData();
-//   }, [id, URL]);
-
-//   //deletes the current profile from database
-//   const handleDelete = async (e) => {
-//     const response = await fetch(URL, {
-//       method: "delete",
-//     });
-//     if (response.status !== 204) console.log("error");
-//     navigate("/");
-//   };
-
-//   const display = pet && (
-//     <div className="bg" style={{ textAlign: "center" }}>
-//       <div style={{ margin: "auto" }}>
-//         <h1>{pet.petName}</h1>
-//         <img src={pet.petPicture} />
-//         <div>
-//           <h4>
-//             {pet.petName} {pet.fosterName}
-//           </h4>
-//         </div>
-
-//         <a href={`/pet/update/${id}`}>Update</a>
-//         <button onClick={handleDelete}>Delete</button>
-//       </div>
-//     </div>
-//   );
-
-//   return <div className="bg" >{display}</div>;
-// };
 
 export default Pet;
   
