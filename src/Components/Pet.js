9f6ba22fef43181dba598cd921398becb7a63e22
cpeppsi <caepps28@gmail.com>
2023-09-29 17:55:19 -0400

    Added styling

--- src/Components/Pet.js
+++ src/Components/Pet.js
@@ -20,36 +20,37 @@ function Pet() {
 
   const display = pets.map(pet => {
     return (
-      <div style={{ textAlign:"center", display:"flex" }}>
-        <Card style={{ 
-          width: '18rem',
-          display: 'inline-block',
-          margin: '5px',
-        }} key={pet._id}>
-          <Card.Img variant="top" src={pet.profilePicture} />
-          <Card.Body>
-            <Card.Title>
-              <Link to={`/pet/${pet._id}`} style={{color:'blue'}}>
-                <h5 style={{textAlign:'center', marginTop: '8px', color:'blue'}}>{pet.petName}</h5>
-              </Link>
-            </Card.Title>
-            <Card.Text>
-              <h5 style={{textAlign:'center', marginTop: '8px', color:'black'}}>Foster Parent: {pet.fosterName}</h5>
-            </Card.Text>
-            <Card.Text>
-              Bio: {pet.petBio}
-            </Card.Text>
-            <Button variant="primary" href="/Adoption">
-                <h5 style={{textAlign:'center', margin: '5px', marginTop: '8px', color:'white'}}>Adopt</h5>
-            </Button>
-          </Card.Body>
-        </Card>
-      </div>
+        <div style={{ textAlign:"center"}}>
+          <Card style={{ 
+            width: '18rem',
+            display: 'inline-block',
+            margin: '5px',
+            backgroundColor: '#f2adf7'
+          }} key={pet._id}>
+            <Card.Img variant="top" src={pet.profilePicture} />
+            <Card.Body>
+              <Card.Title>
+                <Link to={`/pet/${pet._id}`} style={{textDecoration:'inherit'}}>
+                  <h5 style={{textAlign:'center', marginTop: '8px', color:'#c20ecf'}}>{pet.petName}</h5>
+                </Link>
+              </Card.Title>
+              <Card.Text>
+                <h5 style={{textAlign:'center', marginTop: '8px', color:'black'}}>Foster Parent: {pet.fosterName}</h5>
+              </Card.Text>
+              <Card.Text>
+                Bio: {pet.petBio}
+              </Card.Text>
+              <Button href="/Adoption" variant="light">
+                  <h5 style={{textAlign:'center', margin: '5px', marginTop: '8px', color:'#8abcdd'}}>Adopt</h5>
+              </Button>
+            </Card.Body>
+          </Card>
+        </div>
     );
   });
 
   return (
-    <div style={{display: "flex"}}>
+    <div className="wrapper">
       {display}
     </div>
   );
